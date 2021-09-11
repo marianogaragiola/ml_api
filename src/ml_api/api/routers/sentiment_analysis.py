2b4870b78ede52b59a5253859d7f9d6d89415227
marianogaragiola <marianogaragiola@gmail.com>
2021-09-11 18:18:39 -0300

    Update endpoint with the new responses

--- src/ml_api/api/routers/sentiment_analysis.py
+++ src/ml_api/api/routers/sentiment_analysis.py
@@ -4,10 +4,10 @@ sentiment_analysis.py
 import time
 import logging
 
-from fastapi import APIRouter, Request, HTTPException, status
+from fastapi import APIRouter, Request, status, HTTPException
 
-from ml_api.api.schemas import Sentence, SentenceSentiment
-from ml_api.api.models import evaluate_sentiment
+from ml_api.api.schemas import Sentence, SentenceSentiment, Message
+from ml_api.api.models import evaluate_sentiment, ResultNotFound
 
 router = APIRouter()
 
@@ -17,6 +17,7 @@ router = APIRouter()
     summary="Evaluates the sentiment of a sentence",
     description="Given a setence uses a ML model to find its setiment",
     response_model=SentenceSentiment,
+    responses={status.HTTP_400_BAD_REQUEST: {"model": Message}},
     name="predict",
 )
 def get_sentiment(request: Request, sentence: Sentence) -> SentenceSentiment:
@@ -27,5 +28,10 @@ def get_sentiment(request: Request, sentence: Sentence) -> SentenceSentiment:
     sentiment: SentenceSentiment
         Pydantic schema with the sentiment.
     """
-    sentiment = evaluate_sentiment(request, sentence)
+    try:
+        sentiment = evaluate_sentiment(request, sentence)
+    except ResultNotFound as exc:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+        ) from exc
     return sentiment
