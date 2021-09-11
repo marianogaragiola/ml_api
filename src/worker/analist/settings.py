9e92c8be9fec3e2708cc64398c61a2955332aadf
marianogaragiola <marianogaragiola@gmail.com>
2021-09-11 16:22:01 -0300

    Using pydantic base settings for worker settings

--- src/worker/analist/settings.py
+++ src/worker/analist/settings.py
@@ -1,7 +1,21 @@
-THRESHOLDS = [0.25, 0.75]
-SENTIMENTS = ['negative', 'neutral', 'positive']
+from typing import List
+from pydantic import BaseSettings
 
-REDIS_HOST = "redis"
-REDIS_PORT = 6379
-REDIS_DB = 0
-REDIS_QUEUE = "service_queue"
+
+class WorkerSettings(BaseSettings):
+
+    BROKER_HOST: str = "broker"
+    BROKER_PORT: int = 9092
+    BROKER_CONSUMER_TOPIC: str = "task-request"
+    BROKER_PRODUCER_TOPIC: str = "task-computed"
+
+    THRESHOLDS: List[float] = [0.25, 0.75]
+    SENTIMENTS: List[str] = ["negative", "neutral", "positive"]
+
+    REDIS_HOST = "redis"
+    REDIS_PORT = 6379
+    REDIS_DB = 0
+    REDIS_QUEUE = "service_queue"
+
+
+settings = WorkerSettings()
