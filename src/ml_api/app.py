1b5533105cc4963dba2135e7327c07f66148342b
marianogaragiola <marianogaragiola@gmail.com>
2021-09-11 14:59:57 -0300

    Formating code

--- src/ml_api/app.py
+++ src/ml_api/app.py
@@ -4,21 +4,21 @@ This file contains the main app, and some configs.
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from ml_api.api.validators import ApiVersionModel
+from ml_api.api.schemas import ApiVersionModel
 from ml_api.api.routers import sentiment_analysis
 from ml_api.api.handlers import start_app_handler, stop_app_handler
-from ml_api.settings import API_VERSION
+from ml_api.settings import settings
 
 
-__version__ = API_VERSION
+__version__ = settings.API_VERSION
 
 app = FastAPI(
-    title='Machine Learning API',
-    description='Machine Learning API',
-    version=API_VERSION,
+    title="Machine Learning API",
+    description="Machine Learning API",
+    version=settings.API_VERSION,
 )
 
-origins = ['*']
+origins = ["*"]
 
 app.add_middleware(
     CORSMiddleware,
@@ -31,7 +31,7 @@ app.add_middleware(
 
 @app.get("/version", tags=["version"], response_model=ApiVersionModel)
 def get_api_version():
-    """ Get API information. """
+    """Get API information."""
 
     return ApiVersionModel(
         title=app.title, description=app.description, version=app.version
@@ -39,16 +39,8 @@ def get_api_version():
 
 
 app.include_router(
-    sentiment_analysis.router,
-    prefix='/sentiment',
-    tags=['sentiment']
+    sentiment_analysis.router, prefix="/sentiment", tags=["sentiment"]
 )
 
-app.add_event_handler(
-    "startup",
-    start_app_handler(app)
-)
-app.add_event_handler(
-    "shutdown",
-    stop_app_handler(app)
-)
+app.add_event_handler("startup", start_app_handler(app))
+app.add_event_handler("shutdown", stop_app_handler(app))
