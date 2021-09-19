01e9d6b297137bf1ad35ac16e5f3664e2c41e2fd
marianogaragiola <marianogaragiola@gmail.com>
2021-09-19 13:44:17 -0300

    Adding gnunicorn command

--- src/ml_api/cli/manage.py
+++ src/ml_api/cli/manage.py
@@ -1,24 +1,47 @@
 import uvicorn
 import typer
 
+from gunicorn.app.base import BaseApplication
+from ml_api.settings import gunicorn_settings, settings
+
 
 app = typer.Typer()
 
+
+class StandaloneApplication(BaseApplication):
+    """
+    A simple implementation of a Gunicorn Application
+    """
+
+    def __init__(self, app, options=None):
+        self.options = options or {}
+        self.application = app
+        super().__init__()
+
+    def load_config(self):
+        config = {
+            key: value
+            for key, value in self.options.items()
+            if key in self.cfg.settings and value is not None
+        }
+        for key, value in config.items():
+            self.cfg.set(key.lower(), value)
+
+    def load(self):
+        return self.application
+
+
 @app.command()
 def start():
-    typer.echo('Not supported yet')
+    """Start the API with Gunicorn"""
+    StandaloneApplication(settings.APP_MODULE, gunicorn_settings.dict()).run()
 
 
 @app.command()
 def start_reload():
-    typer.echo('Running API with reload mode')
-    uvicorn.run(
-        app='ml_api.app:app',
-        host='0.0.0.0',
-        port=8000,
-        reload=True
-    )
+    typer.echo("Running API with reload mode")
+    uvicorn.run(app="ml_api.app:app", host="0.0.0.0", port=8000, reload=True)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app()
--- src/ml_api/settings.py
+++ src/ml_api/settings.py
@@ -1,4 +1,16 @@
-from pydantic import BaseSettings
+from enum import Enum
+import multiprocessing
+from ipaddress import IPv4Address
+from typing import Optional, Dict, Any
+from pydantic import BaseSettings, PositiveInt, validator
+
+
+class LoggingEnum(str, Enum):
+    critical = "CRITICAL"
+    error = "ERROR"
+    warning = "WARNING"
+    info = "INFO"
+    debug = "DEBUG"
 
 
 class APISettings(BaseSettings):
@@ -9,5 +21,50 @@ class APISettings(BaseSettings):
     BROKER_PRODUCER_TOPIC: str = "task-request"
     BROKER_CONSUMER_TOPIC: str = "task-computed"
 
+    APP_MODULE: str = "ml_api.app:app"
+
+
+class GunicornSettings(BaseSettings):
+    """GunicornSettings Schema"""
+
+    workers_per_core: PositiveInt = 1
+    workers: Optional[PositiveInt] = None
+
+    @validator("workers")
+    def validate_workers(
+        cls, v: Optional[int], values: Dict[str, Any]
+    ) -> PositiveInt:
+        """Validates the number of workers"""
+        if isinstance(v, int):
+            return v
+        core_count = multiprocessing.cpu_count()
+        return core_count * values.get("workers_per_core")
+
+    worker_class: str = "uvicorn.workers.UvicornWorker"
+
+    host: IPv4Address = "0.0.0.0"
+    port: PositiveInt = 8000
+    bind: Optional[str] = None
+
+    @validator("bind", pre=True)
+    def assemble_bind(cls, v: Optional[str], values: Dict[str, Any]) -> Any:
+        if isinstance(v, str):
+            return v
+        return f"{values.get('host')}:{values.get('port')}"
+
+    graceful_timeout: PositiveInt = 120
+    timeout: PositiveInt = 120
+    keepalive: PositiveInt = 5
+
+    # "-" logs to stdout
+    accesslog: str = "-"
+    errorlog: str = "-"
+
+    loglevel: LoggingEnum = LoggingEnum.info
+
+    class Config:
+        use_enum_values = True
+
 
 settings = APISettings()
+gunicorn_settings = GunicornSettings()
