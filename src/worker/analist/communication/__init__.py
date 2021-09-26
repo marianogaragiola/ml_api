0ab14b12debdaea8aa93a68ce3f3bb767829e86c
marianogaragiola <marianogaragiola@gmail.com>
2021-09-26 16:59:04 -0300

    Redis producer in worker

--- src/worker/analist/communication/__init__.py
+++ src/worker/analist/communication/__init__.py
@@ -1,2 +1,2 @@
-from analist.communication.redis import redis_connection
 from analist.communication.kafka import ConsumerBuilder, KafkaSender
+from analist.communication.redis import RedisProducer
--- src/worker/analist/communication/redis.py
+++ src/worker/analist/communication/redis.py
@@ -1,15 +1,21 @@
 import redis
 
-from analist.settings import settings
+from analist.schemas import SentenceSentiment
 
 
-def redis_connection():
-    """
-    Connects to Redis DB and returns the connection
-    """
-    conn = redis.Redis(
-        host=settings.REDIS_HOST,
-        port=settings.REDIS_PORT,
-        db=settings.REDIS_DB,
-    )
-    return conn
+class RedisProducer:
+    _DEFAULT_TTL: int = 6000  # Time to live in seconds
+    _KEY_PREFIX: str = "job_id"
+
+    def __init__(
+        self, host: str, port: int, db: int = 1
+    ):
+        self._conn = redis.Redis(host=host, port=port, db=db)
+
+    def send(self, key: str, value: SentenceSentiment):
+        self._conn.set(
+            name=f"{self._KEY_PREFIX}:{key}",
+            value=value.json(),
+            ex=self._DEFAULT_TTL,
+        )
+
