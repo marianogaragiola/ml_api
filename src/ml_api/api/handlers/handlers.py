3532a856dae7b6144db5f67166c1e4b917a94388
marianogaragiola <marianogaragiola@gmail.com>
2021-09-26 16:56:13 -0300

    Update handlers to create redis consumer

--- src/ml_api/api/handlers/handlers.py
+++ src/ml_api/api/handlers/handlers.py
@@ -2,7 +2,7 @@ from typing import Callable
 
 from fastapi import FastAPI
 
-from ml_api.communication import KafkaSender, KafkaReader
+from ml_api.communication import KafkaSender, RedisConsumer
 from ml_api.settings import settings
 
 
@@ -11,19 +11,16 @@ def _startup_model(app: FastAPI) -> None:
         f"{settings.BROKER_HOST}:{settings.BROKER_PORT}",
         settings.BROKER_PRODUCER_TOPIC,
     )
-    consumer = KafkaReader(
-        f"{settings.BROKER_HOST}:{settings.BROKER_PORT}",
-        settings.BROKER_CONSUMER_TOPIC,
+    redis_conn = RedisConsumer(
+        settings.REDIS_HOST, settings.REDIS_PORT, settings.REDIS_DB
     )
-    consumer.start()
     app.state.producer = producer
-    app.state.consumer = consumer
+    app.state.connection = redis_conn
 
 
 def _shutdown_model(app: FastAPI) -> None:
-    app.state.consumer.stop()
-    app.state.consumer = None
     app.state.producer = None
+    app.state.connections = None
 
 
 def start_app_handler(app: FastAPI) -> Callable:
